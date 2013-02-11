68e9cf28a9527abd83e66da79a0e110391557766
zechocapic <zechocapic@gmail.com>
2013-02-11 19:29:54 +0100

    Changing the welcome

--- TopDownGame/src/org/topdowngame/MainTopDownGame.java
+++ TopDownGame/src/org/topdowngame/MainTopDownGame.java
@@ -5,7 +5,7 @@ public class MainTopDownGame
 	
 	public static void main(String[] args) 
 	{
-		System.out.println("Welcome in this TopDown Game !");
+		System.out.println("Welcome in this magnificent TopDown Game !");
 	}
 
 }
